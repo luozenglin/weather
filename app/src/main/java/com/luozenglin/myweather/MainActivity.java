3163f2d7040a2198a4fad85b742114cad41e58ff
luozenglin <luozenglin@gmail.com>
2018-12-27 14:27:49 +0800

    location

--- app/src/main/java/com/luozenglin/myweather/MainActivity.java
+++ app/src/main/java/com/luozenglin/myweather/MainActivity.java
@@ -1,23 +1,96 @@
 package com.luozenglin.myweather;
+import android.Manifest;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.luozenglin.myweather.utils.Utility;
+
+import java.util.ArrayList;
+
+import java.util.List;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (prefs.getString("weather", null) != null) {
-            Intent intent = new Intent(this, WeatherActivity.class);
-            startActivity(intent);
-            finish();
+        mLocationClient = new LocationClient(getApplicationContext());
+        mLocationClient.registerLocationListener(new MyLocationListener());
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+        }
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!permissionList.isEmpty()) {
+            String [] permissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, 1);
+        } else {
+            requestLocation();
         }
     }
 
+    private void requestLocation() {
+        mLocationClient.start();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            Toast.makeText(this, "必须同意所有权限才能使用本程序", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+                } else {
+                    Toast.makeText(this, "发生未知错误", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+
+
+    public class MyLocationListener implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            String myLocationProvince = location.getProvince();
+            String myLocationCity = location.getCity();
+            String myLocationCounty = location.getDistrict();
+            Log.d("MainActivity","my location:"+myLocationProvince+" "+myLocationCity+" "+myLocationCounty);
+            //String weatherId = Utility.weatherID(MainActivity.this,myLocationProvince,myLocationCity,myLocationCounty);
+            Intent intent = new Intent(MainActivity.this,WeatherActivity.class);
+            //intent.putExtra("weather_id",weatherId);
+            Log.d("MainActivity","will to WeatherActivity");
+            startActivity(intent);
+        }
+    }
 }
--- app/src/main/java/com/luozenglin/myweather/utils/Utility.java
+++ app/src/main/java/com/luozenglin/myweather/utils/Utility.java
@@ -1,6 +1,10 @@
 package com.luozenglin.myweather.utils;
 
+import android.app.Activity;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.google.gson.Gson;
 import com.luozenglin.myweather.common.City;
 import com.luozenglin.myweather.common.County;
@@ -10,6 +14,14 @@ import com.luozenglin.myweather.gson.Weather;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.litepal.crud.DataSupport;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class Utility {
 
@@ -86,5 +98,107 @@ public class Utility {
             e.printStackTrace();
         }
         return null;
+    }
+
+
+    public static String weatherID(Activity activity, String provinceName, String cityName, String countyName) {
+        Province province = queryProvinces(activity,provinceName);
+        Log.d("Utility","searched province:"+province.getProvinceName());
+        City city = queryCities(activity,province,cityName);
+        Log.d("Utility","searched city:"+city.getCityName());
+        String weatherId =  queryCounties(activity,province,city,countyName);
+        Log.d("Utility","searched city:"+weatherId);
+        return weatherId;
+    }
+
+    private static Province queryProvinces(Activity activity, String provinceName) {
+        List<Province> provinceList;
+        Province theProvince = new Province();
+        do {
+            provinceList = DataSupport.findAll(Province.class);
+            if (provinceList.size() > 0) {
+                for (Province province : provinceList) {
+                    if (province.getProvinceName().equals(provinceName)) {
+                        theProvince = province;
+                        break;
+                    }
+                }
+            } else {
+                String address = "http://guolin.tech/api/china";
+                queryFromServer(activity, address, "province", null, null);
+            }
+        } while (provinceList.isEmpty());
+        return theProvince;
+    }
+
+    private static City queryCities(Activity activity, Province province, String cityName) {
+        List<City> cityList;
+        City theCity = new City();
+        do {
+            cityList = DataSupport.where("provinceid = ?", String.valueOf(province.getId())).find(City.class);
+            if (cityList.size() > 0) {
+                for (City city : cityList) {
+                    if (city.getCityName().equals(cityName)) {
+                        theCity = city;
+                        break;
+                    }
+                }
+            } else {
+                int provinceCode = province.getProvinceCode();
+                String address = "http://guolin.tech/api/china/" + provinceCode;
+                queryFromServer(activity, address, "city", province, null);
+            }
+        } while (cityList.isEmpty());
+        return theCity;
+    }
+
+
+    private static String queryCounties(Activity activity, Province province, City city, String countyName) {
+        List<County> countyList;
+        County theCounty = new County();
+        do {
+            countyList = DataSupport.where("cityid = ?", String.valueOf(city.getId())).find(County.class);
+            if (countyList.size() > 0) {
+                for (County county : countyList) {
+                    if (county.getCountyName().equals(countyName)) {
+                        theCounty = county;
+                        break;
+                    }
+                }
+            } else {
+                int provinceCode = province.getProvinceCode();
+                int cityCode = city.getCityCode();
+                String address = "http://guolin.tech/api/china/" + provinceCode + "/" + cityCode;
+                queryFromServer(activity, address, "county", province, city);
+            }
+        } while (countyList.isEmpty());
+        return theCounty.getWeatherId();
+    }
+
+    private static void queryFromServer(final Activity activity, String address, final String type, final Province province, final City city) {
+        HttpUtil.sendOkHttpRequest(address, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(activity, "加载失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)) {
+                    result = Utility.handleProvinceResponse(responseText);
+                } else if ("city".equals(type)) {
+                    result = Utility.handleCityResponse(responseText, province.getId());
+                } else if ("county".equals(type)) {
+                    result = Utility.handleCountyResponse(responseText, city.getId());
+                }
+            }
+        });
     }
 }
